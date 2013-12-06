5730cf5d80baf2ffbc98bce16968e34532860e08
Corbin Combs <corcombs@iupui.edu>
2013-12-06 14:36:39 -0500

    initialize

---
+++ n300App.js
@@ -0,0 +1,39 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+TITLE: navigation.js
+AUTHOR: Corbin Combs
+CREATE DATE: 10/16/2013
+PURPOSE: 
+LAST MODIFIED ON: 10/16/2013
+LAST MODIFIED BY: Corbin Combs
+MODIFICATION HISTORY:
+ 	10/16/2013 - Create date.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ $(document).ready(function(){
+ $(init);
+ $("#button").click(createStory);
+ function init(){
+		$("#button").button();
+		
+}
+function processResult(data, textStatus){
+	$("#story").html(data);
+}
+function createStory(){
+	var prefix = new String();
+	/*if($("#chkBx").is(":checked")){
+		prefix = "Dr. ";
+	}else{
+		if($("#rad1").is(":checked")){
+			prefix = "Ms. ";
+		}else{
+			prefix =	"Mr. ";
+		}
+	}*/
+	var name = $("#firstName").val();
+	var email = $("#email").val();
+	var password =$("#password").val();
+	$.get("http://cs.iupui.edu/~corcombs/n300/n300App/n300App.php",{"password":password,"name":name,"email":email},processResult);
+
+
+
+}});
